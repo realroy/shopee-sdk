b363de6be32dc54a5a63aba82de66542e02a3a10
realroy <roiboon.cc@gmail.com>
2023-06-01 18:49:57 +0700

    feat(core): add logistic ship order

--- packages/core/src/v2-logistic.ship-order.ts
+++ packages/core/src/v2-logistic.ship-order.ts
@@ -3,76 +3,40 @@ import { z } from "zod";
 import { buildQuery } from "./libs";
 import { API_V2_LOGISTIC_GET_SHIPPING_PARAMETER } from "./v2-logistic.constant";
 
-export const logisticGetShippingParameterRequestParameterSchema = z.object({
+export const logisticShipOrderRequestParameterSchema = z.object({
   orderSn: z.string(),
-  packageNumber: z.number().optional(),
-});
-
-export const logisticGetShippingParameterResponseSchema = z.object({
-  error: z.string().optional(),
-  message: z.string().optional(),
-  response: z
+  packageNumber: z.string().optional(),
+  pickup: z
     .object({
-      request_id: z.string(),
-      error: z.string(),
-      message: z.string(),
-      response: z.object({
-        infoNeeded: z.object({
-          dropoff: z.array(z.string()),
-          pickup: z.array(z.string()),
-          nonIntegrated: z.array(z.string()),
-        }),
-        dropoff: z.object({
-          branch_list: z.array(
-            z.object({
-              branch_id: z.number(),
-              region: z.string(),
-              state: z.string(),
-              city: z.string(),
-              address: z.string(),
-              zipcode: z.string(),
-              district: z.string(),
-              town: z.string(),
-            })
-          ),
-          slugList: z.array(
-            z.object({
-              slug: z.string(),
-              slug_name: z.string(),
-            })
-          ),
-        }),
-        pickup: z.object({
-          address_list: z.array(
-            z.object({
-              address_id: z.number(),
-              region: z.string(),
-              state: z.string(),
-              city: z.string(),
-              district: z.string(),
-              town: z.string(),
-              address: z.string(),
-              zipcode: z.string(),
-              address_flag: z.array(z.string()),
-            })
-          ),
-          time_slot_list: z.array(
-            z.object({
-              date: z.number(),
-              time_text: z.string(),
-              pickup_time_id: z.string(),
-            })
-          ),
-        }),
-      }),
+      addressId: z.number().optional(),
+      pickupTimeId: z.string().optional(),
+      trackingNumber: z.string().optional(),
     })
     .optional(),
-  request_id: z.string(),
+  dropoff: z
+    .object({
+      branchId: z.number().optional(),
+      senderRealName: z.string().optional(),
+      trackingNumber: z.string().optional(),
+      slug: z.string().optional(),
+    })
+    .optional(),
+  nonIntegrated: z
+    .object({
+      trackingNumber: z.string().optional(),
+    })
+    .optional(),
+});
+
+export const logisticShipOrderResponseSchema = z.object({
+  error: z.string().optional(),
+  message: z.string().optional(),
+  requestId: z.string(),
 });
 
 export const getShippingParameter = buildQuery({
   path: API_V2_LOGISTIC_GET_SHIPPING_PARAMETER,
-  requestParameterSchema: logisticGetShippingParameterRequestParameterSchema,
-  responseSchema: logisticGetShippingParameterResponseSchema,
+  requestParameterSchema: logisticShipOrderRequestParameterSchema,
+  responseSchema: logisticShipOrderResponseSchema,
   toCamelCase: true,
 });
