4e5ef553855b0e95334dc3ce7f1564ae55c05881
realroy <roiboon.cc@gmail.com>
2023-06-22 04:33:09 +0700

    fix(core): typo

--- packages/core/src/v2-logistic.constant.ts
+++ packages/core/src/v2-logistic.constant.ts
@@ -1,19 +1,20 @@
 import { createConstantsMap } from "./utils";
 
 export const API_V2_LOGISTIC_GET_SHIPPING_PARAMETER =
-  "/api/v2/logistic/get_shipping_parameter";
+  "/api/v2/logistics/get_shipping_parameter";
 export const API_V2_LOGISTIC_SHIP_ORDER = "/api/v2/logistic/ship_order";
 export const API_V2_LOGISTIC_UPDATE_SHIP_ORDER =
   "/api/v2/logistics/update_shipping_order";
 export const API_V2_LOGISTIC_GET_TRACKING_NUMBER =
   "/api/v2/logistics/get_tracking_number";
 export const API_V2_LOGISTIC_GET_SHIPPING_DOCUMENT_PARAMETER =
-  "/api/v2/logistic/get_shipping_document_parameter";
+  "/api/v2/logistics/get_shipping_document_parameter";
 export const API_V2_LOGISTIC_CREATE_SHIPPING_DOCUMENT =
-  "/api/v2/logistic/create_shipping_document";
+  "/api/v2/logistics/create_shipping_document";
 export const API_V2_LOGISTIC_GET_SHIPPING_DOCUMENT_RESULT =
   "/api/v2/logistics/get_shipping_document_result";
-export const API_V2_LOGISTIC_DOWNLOAD_SHIPPING_DOCUMENT = '/api/v2/logistics/download_shipping_document'
+export const API_V2_LOGISTIC_DOWNLOAD_SHIPPING_DOCUMENT =
+  "/api/v2/logistics/download_shipping_document";
 
 export const V2_LOGISTIC_GET_TRACKING_NUMBER_RESPONSE_OPTIONAL_FIELDS = [
   "plp_number",
@@ -31,4 +32,8 @@ export const V2_LOGISTIC_SHIPPING_DOCUMENT_TYPES = [
   "THERMAL_JOB_AIR_WAYBILL",
 ] as const;
 
-export const V2_LOGISTIC_SHIPPING_STATUSES = ["READY", "FAILED", "PROCESSING"] as const;
+export const V2_LOGISTIC_SHIPPING_STATUSES = [
+  "READY",
+  "FAILED",
+  "PROCESSING",
+] as const;
