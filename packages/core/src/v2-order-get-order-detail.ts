5ff49b61b63f6e3326459fd7ea40e0acd141ec33
realroy <roiboon.cc@gmail.com>
2023-07-07 18:00:14 +0700

    fix: invalid order detail response

--- packages/core/src/v2-order-get-order-detail.ts
+++ packages/core/src/v2-order-get-order-detail.ts
@@ -86,7 +86,7 @@ export const getOrderDetailResponseSchema = z.object({
           note: z.string(),
           noteUpdateTime: z.number(),
           orderSn: z.string(),
-          orderStatus: z.array(z.enum(V2_ORDER_STATUS)),
+          orderStatus: z.enum(V2_ORDER_STATUS),
           packageList: z.array(
             z.object({
               packageNumber: z.string(),
