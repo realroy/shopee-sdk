454e86a621a8655f69022016c13ff35469c9f088
realroy <roiboon.cc@gmail.com>
2023-07-11 03:18:13 +0700

    fix(core): invalid refresh token parameter

--- packages/core/src/v2-auth-refresh-access-token.ts
+++ packages/core/src/v2-auth-refresh-access-token.ts
@@ -4,11 +4,10 @@ import { ShopeeContext, buildMutation } from "./libs";
 import { API_V2_AUTH_REFRESH_ACCESS_TOKEN_PATH } from "./v2-auth.constant";
 
 export const refreshAccessTokenRequestParametersSchema = z.object({
-  code: z.string(),
+  refreshToken: z.string(),
   partnerId: z.number().optional(),
   shopId: z.number().optional(),
   merchantId: z.number().optional(),
-  refreshToken: z.string(),
 });
 
 export const refreshAccessTokenResponseSchema = z.object({
